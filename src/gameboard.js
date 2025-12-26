afee26ea513a120aadee7d8b40f67ff76048764c
Augustine Ennin <augustineennin206@gmail.com>
2025-12-26 14:50:23 -0500

    Add Ship and Gameboard logic

---
+++ src/gameboard.js
@@ -0,0 +1,33 @@
+import Ship from './ship';
+
+class Gameboard {
+    constructor() {
+        this.ships = [];
+        this.missedAttacks = [];
+    }
+
+    placeShip(length, coordinates) {
+        const ship = new Ship(length);
+        this.ships.push({ship, coordinates});
+    }
+
+    receiveAttack([x, y]) {
+        for (const obj of this.ships) {
+            for (const coord of obj.coordinates) {
+                if (coord[0] === x && coord[1] === y) {
+                    obj.ship.hit();
+                    return 'hit';
+                }
+            }
+        }
+
+        this.missedAttacks.push([x, y]);
+        return 'miss';
+    }
+
+    allShipsSunk() {
+        return this.ships.every(obj => obj.ship.isSunk());
+    }
+}
+
+export default Gameboard;
