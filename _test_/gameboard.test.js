c3197e56c3ff5af973b8f7388acdeb9b15ea35aa
Augustine Ennin <augustineennin206@gmail.com>
2025-12-26 14:51:05 -0500

    Add unit tests for ship and Gameboard

---
+++ _test_/gameboard.test.js
@@ -0,0 +1,33 @@
+import Gameboard from "../src/gameboard";
+
+describe('Gameboard', () => {
+    let board;
+
+    beforeEach(() => {
+        board = new Gameboard();
+    });
+
+    test('places ships at specific coordinates', () => {
+        board.placeShip(3, [[0,0], [0,1], [0,2]]);
+        expect(board.ships.length).toBe(1);
+    });
+
+    test('records a hit when attack hits a ship', () => {
+        board.placeShip(2, [[1,1], [1,2]]);
+        const result = board.receiveAttack([1,1]);
+        expect(result).toBe('hit');
+    });
+
+    test('records missed attacks', () => {
+        board.placeShip(2, [[1,1], [1,2]]);
+        board.receiveAttack([5,5]);
+        expect(board.missedAttacks).toContainEqual([5,5]);
+    });
+
+    test('reports all ships sunk correctly', () => {
+        board.placeShip(2, [[0,0], [0,1]]);
+        board.receiveAttack([0,0]);
+        board.receiveAttack([0,1]);
+        expect(board.allShipsSunk()).toBe(true);
+    });
+});
---
+++ _test_/ship.test.js
@@ -0,0 +1,23 @@
+import Ship from "./src/ship";
+
+describe('Ship', () => {
+    test('stores its length', () => {
+        const ship = Ship(3);
+        expect(ship.length).toBe(3);
+    });
+
+    test('hit() increases the number of hits', () => {
+        const ship = Ship(3);
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+    });
+
+    test('isSunk() returns false when hits are less than length', () => {
+        const ship = Ship(4);
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(false);
+    });
+});
