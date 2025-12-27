e2b7361bafe55b4162a2d5f52751551ff6983533
Augustine Ennin <augustineennin206@gmail.com>
2025-12-27 15:17:32 -0500

    Add Player factory with gameboard ownership and attacks

---
+++ __test__/player.test.js
@@ -0,0 +1,35 @@
+import Player from "../src/player";
+import Gameboard from "../src/gameboard";
+
+describe('Player', () => {
+    test('player has a gameboard', () => {
+        const player = Player();
+        expect(player.gameboard).toBeInstanceOf(Gameboard);
+    });
+
+    test('player can attack enemy gameboard', () => {
+        const player = Player();
+        const enemy = Player();
+
+        enemy.gameboard.placeShip(2, [[0,0], [0, 1]]);
+        player.attack(enemy.gameboard, [0,0]);
+
+        expect(enemy.gameboard.missedAttacks.length).toBe(0);
+    });
+
+    test('computer player makes random attacks', () => {
+        const computer = Player(true);
+        const enemy = Player();
+
+        computer.randomAttack(enemy.gameboard);
+
+        const totalAttacks =
+        enemy.gameboard.missedAttacks.length + 
+        enemy.gameboard.ships.reduce(
+            (sum, s) => sum + s.ship.isSunk(),
+            0
+        );
+
+        expect(totalAttacks).toBeGreaterThanOrEqual(0);
+    });
+});
---
+++ src/player.js
@@ -0,0 +1,39 @@
+import Gameboard from "./gameboard";
+
+const Player = (isComputer = false) => {
+    const gameboard = new Gameboard();
+    const previousAttacks = [];
+
+    const attack = (enemyBoard, coordinates) =>{
+        enemyBoard.receiveAttack(coordinates);
+    };
+
+    const randomAttack = (enemyBoard) => {
+        if (!isComputer) return;
+
+        let coord;
+
+        do {
+            coord = [
+                Math.floor(Math.random() * 10),
+                Math.floor(Math.random() * 10),
+            ];
+        } while (
+            previousAttacks.some(
+                ([x, y]) => x === coord[0] && y === coord[1]
+            )
+        );
+
+        previousAttacks.push(coord);
+        enemyBoard.receiveAttack(coord);
+    };
+
+    return {
+        gameboard,
+        attack,
+        randomAttack,
+        isComputer,
+    };
+};
+
+export default Player;
